a73ffc4b0dda1b5f553a953a2aff0f108716b5c6
머환임 <udsward@gmail.com>
2023-04-19 00:31:30 +0900

    Retrofit setting

---
+++ app/src/main/java/com/sopt/smeem/core/network/RequestInterceptor.kt
@@ -0,0 +1,19 @@
+package com.sopt.smeem.core.network
+
+import okhttp3.Interceptor
+import okhttp3.Response
+
+class RequestInterceptor(
+    val accessToken: String,
+    val refreshToken: String
+) : Interceptor {
+    override fun intercept(chain: Interceptor.Chain): Response = chain.proceed(
+        chain.request().newBuilder().apply {
+            header(API_ACCESS_TOKEN_HEADER, "Bearer $accessToken")
+            header(API_REFRESH_TOKEN_HEADER, "Bearer $refreshToken") // TODO : 서버에서 정의되는 refresh token 정책에 맞춰 구성
+        }.build()
+    )
+}
+
+internal val API_ACCESS_TOKEN_HEADER = "Authorization"
+internal val API_REFRESH_TOKEN_HEADER = "not yet" // TODO
