8568592a6a6439a4356a1b3b0fa16bbf30ef4a1c
머환임 <udsward@gmail.com>
2023-05-01 14:55:13 +0900

    implement kakao auth

--- app/src/main/java/com/sopt/smeem/domain/main/AuthActivity.kt
+++ app/src/main/java/com/sopt/smeem/domain/main/AuthActivity.kt
@@ -2,11 +2,29 @@ package com.sopt.smeem.domain.main
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.sopt.smeem.R
+import com.sopt.smeem.databinding.ActivityAuthBinding
 
 class AuthActivity : AppCompatActivity() {
+    lateinit var binding: ActivityAuthBinding
+    lateinit var bs: AuthBottomSheet
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // TODO
+        binding = ActivityAuthBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setUpBottomSheet()
+        onTouch()
+    }
+
+    private fun setUpBottomSheet() {
+        bs = AuthBottomSheet()
+    }
+
+
+    private fun onTouch() {
+        binding.btnAuthStart.setOnClickListener {
+            bs.show(supportFragmentManager, AuthBottomSheet.TAG)
+        }
     }
 }
---
+++ app/src/main/java/com/sopt/smeem/domain/main/AuthBottomSheet.kt
@@ -0,0 +1,71 @@
+package com.sopt.smeem.domain.main
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.kakao.sdk.user.UserApiClient
+import com.sopt.smeem.R
+import com.sopt.smeem.databinding.BottomSheetAuthBinding
+
+class AuthBottomSheet : BottomSheetDialogFragment() {
+    var _binding : BottomSheetAuthBinding? = null
+    private val binding: BottomSheetAuthBinding
+     get() = requireNotNull(_binding)
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = BottomSheetAuthBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val context = requireContext()
+        // TODO : 구현만 확인, 이후 코드 정리 및 절차별 분기 구현 필요
+        binding.btnKakao.setOnClickListener {
+            if(UserApiClient.instance.isKakaoTalkLoginAvailable(context)) {
+                UserApiClient.instance.loginWithKakaoTalk(requireContext()) { token, error ->
+                    if(error != null) {
+                        // login failed
+                        Toast.makeText(context, "로그인 실패", Toast.LENGTH_SHORT).show()
+                    }
+                    else if (token != null) {
+                        // login succeed
+                        Toast.makeText(context, "로그인 성공", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+            else {
+                UserApiClient.instance.loginWithKakaoAccount(context, callback = {token, error ->
+                    run {
+                        if (error != null) {
+                            // login failed
+                            Toast.makeText(context, "로그인 실패", Toast.LENGTH_SHORT).show()
+                        } else if (token != null) {
+                            // login succeed
+                            Toast.makeText(context, "로그인 성공", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                })
+            }
+
+        }
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+    companion object {
+        const val TAG = "BottomSheetAuth"
+    }
+}
