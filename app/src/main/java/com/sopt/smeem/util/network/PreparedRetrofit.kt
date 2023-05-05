98113722a759e53c95eb22148bba5a279d757298
머환임 <udsward@gmail.com>
2023-05-05 19:26:12 +0900

    fix preparedRetrofit

--- app/src/main/java/com/sopt/smeem/util/network/PreparedRetrofit.kt
+++ app/src/main/java/com/sopt/smeem/util/network/PreparedRetrofit.kt
@@ -1,40 +1,47 @@
 package com.sopt.smeem.util.network
 
+import com.sopt.smeem.BuildConfig.API_SERVER_URL
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
 
 object PreparedRetrofit {
     // 인증 전 혹은 비회원 api Retrofit
-    val apiServerRetrofitForAnonymous = Retrofit.Builder().apply {
-        // baseUrl(API_SERVER_URL) TODO : set API_SERVER_URL localproperties
-        client(okHttpClientExcludeAuth)
-    }.build()
-
-    private val okHttpClientExcludeAuth = OkHttpClient.Builder().apply {
-        connectTimeout(10, TimeUnit.SECONDS)
-        writeTimeout(5, TimeUnit.SECONDS)
-        readTimeout(5, TimeUnit.SECONDS)
-    }.build()
+    val apiServerRetrofitForAnonymous by lazy {
+        Retrofit.Builder()
+            .baseUrl(API_SERVER_URL)
+            .client(
+                OkHttpClient.Builder().apply {
+                    connectTimeout(10, TimeUnit.SECONDS)
+                    writeTimeout(5, TimeUnit.SECONDS)
+                    readTimeout(5, TimeUnit.SECONDS)
+                }.build()
+            )
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
 
     // 인증 후 api Retrofit
-    val apiServerRetrofitForAuthentication = Retrofit.Builder().apply {
-        // baseUrl(API_SERVER_URL)
-        client(okHttpClientIncludeAuth)
-    }.build()
+    val apiServerRetrofitForAuthentication by lazy {
+        Retrofit.Builder().apply {
+            // baseUrl(API_SERVER_URL)
+            client(
+                OkHttpClient.Builder().apply {
+                    connectTimeout(10, TimeUnit.SECONDS)
+                    writeTimeout(5, TimeUnit.SECONDS)
+                    readTimeout(5, TimeUnit.SECONDS)
 
-    private val okHttpClientIncludeAuth = OkHttpClient.Builder().apply {
-        connectTimeout(10, TimeUnit.SECONDS)
-        writeTimeout(5, TimeUnit.SECONDS)
-        readTimeout(5, TimeUnit.SECONDS)
-
-        // TODO : local storage 로 부터 api server access token 을 가져오는로직 필요
-        // if 분기 필요
-        addInterceptor(
-            RequestInterceptor(
-                accessToken = "",
-                refreshToken = ""
+                    // TODO : local storage 로 부터 api server access token 을 가져오는로직 필요
+                    // if 분기 필요
+                    addInterceptor(
+                        RequestInterceptor(
+                            accessToken = "",
+                            refreshToken = ""
+                        )
+                    ) // TODO : local storage 에 인증토큰이 적재되어있다면, interceptor 에 담아준다.
+                }.build()
             )
-        ) // TODO : local storage 에 인증토큰이 적재되어있다면, interceptor 에 담아준다.
-    }.build()
+        }.build()
+    }
 }
