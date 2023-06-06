d781f39455ee4d15df517cdc77522cf96dae75ec
watermin <tnals08019@gmail.com>
2023-06-07 00:59:07 +0900

    [ui] foreign write toolbar, random topic

---
+++ app/src/main/java/com/sopt/smeem/presentation/write/foreign/ForeignWriteActivity.kt
@@ -0,0 +1,10 @@
+package com.sopt.smeem.presentation.write.foreign
+
+import com.sopt.smeem.R
+import com.sopt.smeem.databinding.ActivityForeignWriteBinding
+import com.sopt.smeem.presentation.BindingActivity
+
+class ForeignWriteActivity :
+    BindingActivity<ActivityForeignWriteBinding>(R.layout.activity_foreign_write) {
+
+}
