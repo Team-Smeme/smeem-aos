0a037bce69eebf29c5f865025173861e0f3c70f8
watermin <tnals08019@gmail.com>
2023-06-19 20:26:43 +0900

    [feat] toggle random topic

--- app/src/main/java/com/sopt/smeem/presentation/write/foreign/ForeignWriteActivity.kt
+++ app/src/main/java/com/sopt/smeem/presentation/write/foreign/ForeignWriteActivity.kt
@@ -1,5 +1,6 @@
 package com.sopt.smeem.presentation.write.foreign
 
+import android.view.View
 import androidx.activity.viewModels
 import com.sopt.smeem.R
 import com.sopt.smeem.databinding.ActivityForeignWriteBinding
@@ -12,9 +13,42 @@ class ForeignWriteActivity :
 
     override fun constructLayout() {
         with(binding) {
-            etForeignWrite.requestFocus()
+            // databinding
             vm = viewModel
             lifecycleOwner = this@ForeignWriteActivity
+            // ui
+            etForeignWrite.requestFocus()
+            layoutForeignWriteRandomTopic.layoutSection.visibility = View.GONE
+        }
+    }
+
+    override fun addListeners() {
+        setTopicVisibility()
+        refreshTopic()
+    }
+
+    private fun setTopicVisibility() {
+        with(binding) {
+            layoutForeignWriteBottomToolbar.cbRandomTopic.setOnCheckedChangeListener { _, isChecked ->
+                when (isChecked) {
+                    true -> {
+                        layoutForeignWriteBottomToolbar.tvRandomTopicLabel.setTextColor(resources.getColor(R.color.point, null))
+                        // TODO: 새로운 랜덤 주제 불러오기
+//                    layoutForeignWriteRandomTopic.tvContent.text =
+                        layoutForeignWriteRandomTopic.layoutSection.visibility = View.VISIBLE
+                    }
+                    false -> {
+                        layoutForeignWriteBottomToolbar.tvRandomTopicLabel.setTextColor(resources.getColor(R.color.gray_500, null))
+                        layoutForeignWriteRandomTopic.layoutSection.visibility = View.GONE
+                    }
+                }
+            }
+        }
+    }
+
+    private fun refreshTopic() {
+        binding.layoutForeignWriteRandomTopic.btnRefresh.setOnClickListener {
+            // TODO: 새로운 랜덤 주제 불러오기
         }
     }
 
