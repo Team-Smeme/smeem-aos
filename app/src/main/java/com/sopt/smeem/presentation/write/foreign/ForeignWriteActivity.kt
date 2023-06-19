f8ba7f131bbf377e206fd875ded544b70c219321
watermin <tnals08019@gmail.com>
2023-06-19 19:02:37 +0900

    [feat] show keyboard automatically

--- app/src/main/java/com/sopt/smeem/presentation/write/foreign/ForeignWriteActivity.kt
+++ app/src/main/java/com/sopt/smeem/presentation/write/foreign/ForeignWriteActivity.kt
@@ -1,10 +1,21 @@
 package com.sopt.smeem.presentation.write.foreign
 
+import androidx.activity.viewModels
 import com.sopt.smeem.R
 import com.sopt.smeem.databinding.ActivityForeignWriteBinding
 import com.sopt.smeem.presentation.BindingActivity
 
 class ForeignWriteActivity :
     BindingActivity<ActivityForeignWriteBinding>(R.layout.activity_foreign_write) {
+
+    private val viewModel by viewModels<ForeignWriteViewModel>()
+
+    override fun constructLayout() {
+        with(binding) {
+            etForeignWrite.requestFocus()
+            vm = viewModel
+            lifecycleOwner = this@ForeignWriteActivity
+        }
+    }
 
 }
