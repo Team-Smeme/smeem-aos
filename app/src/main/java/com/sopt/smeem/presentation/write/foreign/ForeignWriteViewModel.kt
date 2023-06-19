0764352a112b30e3d59d826f9dca8a058ff0c5c5
watermin <tnals08019@gmail.com>
2023-06-19 19:04:33 +0900

    [ui] databinding temp commit

---
+++ app/src/main/java/com/sopt/smeem/presentation/write/foreign/ForeignWriteViewModel.kt
@@ -0,0 +1,13 @@
+package com.sopt.smeem.presentation.write.foreign
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.sopt.smeem.domain.model.auth.LoginResult
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class ForeignWriteViewModel @Inject constructor(): ViewModel() {
+
+}
