4f7c254b5dad75f05515c5536a2dedbe9c98cb79
watermin <tnals08019@gmail.com>
2023-06-21 00:07:28 +0900

    [feat] set up topic, diary databinding

--- app/src/main/java/com/sopt/smeem/presentation/write/foreign/ForeignWriteViewModel.kt
+++ app/src/main/java/com/sopt/smeem/presentation/write/foreign/ForeignWriteViewModel.kt
@@ -3,11 +3,12 @@ package com.sopt.smeem.presentation.write.foreign
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.sopt.smeem.domain.model.auth.LoginResult
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class ForeignWriteViewModel @Inject constructor(): ViewModel() {
+class ForeignWriteViewModel @Inject constructor() : ViewModel() {
+    val topic = MutableLiveData("")
+    val diary = MutableLiveData("")
 
 }
