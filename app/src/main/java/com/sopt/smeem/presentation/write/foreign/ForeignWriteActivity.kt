a64dd258e31159007a96e98c091133f6c034811c
watermin <tnals08019@gmail.com>
2023-06-20 21:19:23 +0900

    [feat] implement random topic tooltip

--- app/src/main/java/com/sopt/smeem/presentation/write/foreign/ForeignWriteActivity.kt
+++ app/src/main/java/com/sopt/smeem/presentation/write/foreign/ForeignWriteActivity.kt
@@ -2,9 +2,11 @@ package com.sopt.smeem.presentation.write.foreign
 
 import android.view.View
 import androidx.activity.viewModels
+import androidx.lifecycle.LifecycleOwner
 import com.sopt.smeem.R
 import com.sopt.smeem.databinding.ActivityForeignWriteBinding
 import com.sopt.smeem.presentation.BindingActivity
+import com.sopt.smeem.util.TooltipUtil.createTopicTooltip
 
 class ForeignWriteActivity :
     BindingActivity<ActivityForeignWriteBinding>(R.layout.activity_foreign_write) {
@@ -19,6 +21,7 @@ class ForeignWriteActivity :
             // ui
             etForeignWrite.requestFocus()
             layoutForeignWriteRandomTopic.layoutSection.visibility = View.GONE
+            showTooltip(lifecycleOwner)
         }
     }
 
@@ -27,18 +30,30 @@ class ForeignWriteActivity :
         refreshTopic()
     }
 
+    private fun showTooltip(owner: LifecycleOwner?) {
+        // TODO: 최초 실행 여부 조건문
+        binding.layoutForeignWriteBottomToolbar.cbRandomTopic.createTopicTooltip(
+            this@ForeignWriteActivity, owner
+        )
+    }
+
     private fun setTopicVisibility() {
         with(binding) {
             layoutForeignWriteBottomToolbar.cbRandomTopic.setOnCheckedChangeListener { _, isChecked ->
                 when (isChecked) {
                     true -> {
-                        layoutForeignWriteBottomToolbar.tvRandomTopicLabel.setTextColor(resources.getColor(R.color.point, null))
+                        layoutForeignWriteBottomToolbar.tvRandomTopicLabel.setTextColor(
+                            resources.getColor(R.color.point, null)
+                        )
                         // TODO: 새로운 랜덤 주제 불러오기
-//                    layoutForeignWriteRandomTopic.tvContent.text =
+//                        layoutForeignWriteRandomTopic.tvContent.text =
                         layoutForeignWriteRandomTopic.layoutSection.visibility = View.VISIBLE
                     }
+
                     false -> {
-                        layoutForeignWriteBottomToolbar.tvRandomTopicLabel.setTextColor(resources.getColor(R.color.gray_500, null))
+                        layoutForeignWriteBottomToolbar.tvRandomTopicLabel.setTextColor(
+                            resources.getColor(R.color.gray_500, null)
+                        )
                         layoutForeignWriteRandomTopic.layoutSection.visibility = View.GONE
                     }
                 }
