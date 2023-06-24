4d3916c44b6b901a0f6fd464aa3e8530885c9454
머환임 <udsward@gmail.com>
2023-06-24 12:39:12 +0900

    마이페이지 조회 API server spec 에 맞게 fix

--- app/src/main/java/com/sopt/smeem/domain/model/Badge.kt
+++ app/src/main/java/com/sopt/smeem/domain/model/Badge.kt
@@ -5,7 +5,7 @@ import com.sopt.smeem.data.model.response.MyPageResponse
 data class Badge(
     val badgeId: Long,
     val title: String,
-    val description: String,
+    val description: String?,
     val badgeType: BadgeType,
     val imageUrl: String = "https://github-production-user-asset-6210df.s3.amazonaws.com/120551217/244917999-f9f63817-8b1a-483c-b0e1-4935c96e2d03.png"
 ) {
