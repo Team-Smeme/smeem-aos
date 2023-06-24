8a3581c18f3ff24d10ff974e8af5f8669d873786
watermin <tnals08019@gmail.com>
2023-06-24 15:41:24 +0900

    [feat] implement done button
    * change to point color when foreign language is included in edittext
    * show snackbar when there is no foreign language

--- app/src/main/java/com/sopt/smeem/presentation/write/foreign/ForeignWriteViewModel.kt
+++ app/src/main/java/com/sopt/smeem/presentation/write/foreign/ForeignWriteViewModel.kt
@@ -3,6 +3,7 @@ package com.sopt.smeem.presentation.write.foreign
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.map
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
@@ -10,5 +11,12 @@ import javax.inject.Inject
 class ForeignWriteViewModel @Inject constructor() : ViewModel() {
     val topic = MutableLiveData("")
     val diary = MutableLiveData("")
+    val isValidDiary: LiveData<Boolean> = diary.map { isValidDiaryFormat(it) }
 
+    private fun isValidDiaryFormat(diary: String): Boolean {
+        if (diary.replace("[^a-zA-Z]".toRegex(), "").isNotEmpty()) {
+            return true
+        }
+        return false
+    }
 }
