8e2c9ae5eb0d86fb3f708ed2dddb96dffae45f93
watermin <tnals08019@gmail.com>
2023-06-26 07:33:46 +0900

    [feat] implement step1

--- app/src/main/java/com/sopt/smeem/presentation/write/natiive/NativeWriteStep1ViewModel.kt
+++ app/src/main/java/com/sopt/smeem/presentation/write/natiive/NativeWriteStep1ViewModel.kt
@@ -1,9 +1,22 @@
 package com.sopt.smeem.presentation.write.natiive
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.map
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
-class NativeWriteStep1ViewModel: ViewModel() {
+@HiltViewModel
+class NativeWriteStep1ViewModel @Inject constructor() : ViewModel() {
     val topic = MutableLiveData("")
     val diary = MutableLiveData("")
+    val isValidDiary: LiveData<Boolean> = diary.map { isValidDiaryFormat(it) }
+
+    private fun isValidDiaryFormat(diary: String): Boolean {
+        if (diary.filterNot { it.isWhitespace() }.isNotEmpty()) {
+            return true
+        }
+        return false
+    }
 }
