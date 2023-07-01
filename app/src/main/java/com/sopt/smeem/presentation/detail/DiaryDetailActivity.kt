7f822b214d2af37d7fe573cfd4e983d406599168
watermin <tnals08019@gmail.com>
2023-07-01 13:21:06 +0900

    [ui] 일기 상세보기

---
+++ app/src/main/java/com/sopt/smeem/presentation/detail/DiaryDetailActivity.kt
@@ -0,0 +1,10 @@
+package com.sopt.smeem.presentation.detail
+
+import com.sopt.smeem.R
+import com.sopt.smeem.databinding.ActivityDiaryDetailBinding
+import com.sopt.smeem.presentation.BindingActivity
+
+class DiaryDetailActivity :
+    BindingActivity<ActivityDiaryDetailBinding>(R.layout.activity_diary_detail) {
+
+}
