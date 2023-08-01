e75eb7febb106dfc04434ad6354eca9a88eb5131
HaeTi <t9h9@naver.com>
2023-08-01 23:08:11 +0900

    Add setOnSingleClickListener

--- app/src/main/java/com/sopt/smeem/util/ViewExt.kt
+++ app/src/main/java/com/sopt/smeem/util/ViewExt.kt
@@ -24,3 +24,17 @@ fun View.showSnackbar(
         .setAnchorView(anchor)
         .show()
 }
+
+inline fun View.setOnSingleClickListener(
+    delay: Long = 500L,
+    crossinline block: (View) -> Unit,
+) {
+    var previousClickedTime = 0L
+    setOnClickListener { view ->
+        val clickedTime = System.currentTimeMillis()
+        if (clickedTime - previousClickedTime >= delay) {
+            block(view)
+            previousClickedTime = clickedTime
+        }
+    }
+}
