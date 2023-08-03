0b7f82eb51095a8f3a81d3432fb18f4a66491813
HaeTi <t9h9@naver.com>
2023-08-03 21:45:02 +0900

    타켓팅에 따라 버튼 visibility 조정

--- app/src/main/java/com/sopt/smeem/presentation/home/HomeActivity.kt
+++ app/src/main/java/com/sopt/smeem/presentation/home/HomeActivity.kt
@@ -1,6 +1,7 @@
 package com.sopt.smeem.presentation.home
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.viewModels
 import com.sopt.smeem.R
 import com.sopt.smeem.databinding.ActivityHomeBinding
@@ -14,16 +15,30 @@ import java.time.format.DateTimeFormatter
 class HomeActivity : BindingActivity<ActivityHomeBinding>(R.layout.activity_home) {
 
     private val homeViewModel by viewModels<HomeViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        initView()
         setInitListener()
-        setTargetMonthTitle()
+    }
+
+    private fun initView() {
+        binding.tvTargetMonth.text = LocalDate.now().format(
+            DateTimeFormatter.ofPattern(TARGET_MONTH_PATTERN),
+        )
     }
 
     private fun setInitListener() {
         binding.weeklyCalendar.setOnWeeklyDayClickListener { view, date ->
-            binding.tvTargetMonth.text = date.format(DateTimeFormatter.ofPattern(TARGET_MONTH_PATTERN))
+            binding.tvTargetMonth.text =
+                date.format(DateTimeFormatter.ofPattern(TARGET_MONTH_PATTERN))
+
+            binding.btnWriteDiary.visibility = if (LocalDate.now().isEqual(date)) {
+                View.VISIBLE
+            } else {
+                View.GONE
+            }
         }
 
         binding.weeklyCalendar.setOnWeeklyCalendarSwipeListener(object :
