27468b4f18cf63169443f14980d3defdb5459dfa
HaeTi <t9h9@naver.com>
2023-08-05 17:52:37 +0900

    일기목록조회 api 연동

--- app/src/main/java/com/sopt/smeem/presentation/home/HomeViewModel.kt
+++ app/src/main/java/com/sopt/smeem/presentation/home/HomeViewModel.kt
@@ -1,5 +1,35 @@
 package com.sopt.smeem.presentation.home
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.sopt.smeem.Authenticated
+import com.sopt.smeem.domain.model.DiarySummaries
+import com.sopt.smeem.domain.repository.DiaryRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class HomeViewModel : ViewModel()
+@HiltViewModel
+class HomeViewModel @Inject constructor(
+    @Authenticated private val diaryRepository: DiaryRepository,
+) : ViewModel() {
+
+    private val _responseDiaries: MutableLiveData<DiarySummaries> = MutableLiveData()
+    val responseDiaries: LiveData<DiarySummaries>
+        get() = _responseDiaries
+
+    fun getDiaries(start: String, end: String) {
+        viewModelScope.launch {
+            kotlin.runCatching {
+                diaryRepository.getDiaries(start, end)
+            }.fold({
+                _responseDiaries.value = it.getOrNull()
+            }, {
+                Log.e("message", it.message.toString())
+            })
+        }
+    }
+}
