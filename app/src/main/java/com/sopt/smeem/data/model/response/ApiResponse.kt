dd5d9a8ad0eb3030717f2c09c5fea0fb671fb545
HaeTi <t9h9@naver.com>
2023-08-08 23:12:38 +0900

    status nullable 하게 변경

--- app/src/main/java/com/sopt/smeem/data/model/response/ApiResponse.kt
+++ app/src/main/java/com/sopt/smeem/data/model/response/ApiResponse.kt
@@ -1,8 +1,8 @@
 package com.sopt.smeem.data.model.response
 
 data class ApiResponse<RESULT>(
-    val status: Int,
+    val status: Int?,
     val success: Boolean,
     val message: String?,
-    val data: RESULT?
+    val data: RESULT?,
 )
