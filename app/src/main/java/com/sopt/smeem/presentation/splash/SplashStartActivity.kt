c1114246693225ce7bb8d8cd29d79895c097c364
머환임 <udsward@gmail.com>
2023-08-13 11:11:16 +0900

    StartSplash 구현 (android 12+ splash 대응)

--- app/src/main/java/com/sopt/smeem/presentation/splash/SplashStartActivity.kt
+++ app/src/main/java/com/sopt/smeem/presentation/splash/SplashStartActivity.kt
@@ -1,23 +1,33 @@
 package com.sopt.smeem.presentation.splash
 
 import android.content.Intent
+import android.os.Bundle
 import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.sopt.smeem.R
-import com.sopt.smeem.databinding.ActivitySplashStartBinding
-import com.sopt.smeem.presentation.BindingActivity
 import com.sopt.smeem.presentation.home.HomeActivity
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class SplashStartActivity :
-    BindingActivity<ActivitySplashStartBinding>(R.layout.activity_splash_start) {
+class SplashStartActivity() : AppCompatActivity() {
     private val vm: SplashVM by viewModels()
 
-    override fun constructLayout() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        installSplashScreen()
+
+        setContentView(R.layout.activity_splash_start)
+
+        constructLayout()
+        addObservers()
+    }
+
+    fun constructLayout() {
         vm.checkAuthed()
     }
 
-    override fun addObservers() {
+    fun addObservers() {
         observeAuthed()
     }
 
