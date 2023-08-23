eaf80ce9ae449db46b01ba0b01b85f7e5ed40242
HaeTi <t9h9@naver.com>
2023-08-23 16:25:11 +0900

    뷰모델의 코루틴 제거

--- app/src/main/java/com/sopt/smeem/presentation/home/HomeViewModel.kt
+++ app/src/main/java/com/sopt/smeem/presentation/home/HomeViewModel.kt
@@ -4,13 +4,10 @@ import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import com.sopt.smeem.domain.model.DiarySummaries
 import com.sopt.smeem.domain.model.DiarySummary
-import com.sopt.smeem.domain.model.RetrievedBadge
 import com.sopt.smeem.domain.repository.DiaryRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -29,29 +26,25 @@ class HomeViewModel @Inject constructor(
     private val _responseDateDiary: MutableLiveData<DiarySummary?> = MutableLiveData()
     val responseDateDiary: LiveData<DiarySummary?> get() = _responseDateDiary
 
-    fun getDiaries(start: String, end: String) {
-        viewModelScope.launch {
-            kotlin.runCatching {
-                diaryRepository.getDiaries(start, end)
-            }.fold({
-                Log.e("message", it.getOrNull().toString())
-                _responseDiaries.value = it.getOrNull()
-            }, {
-                Log.e("message", it.message.toString())
-            })
-        }
+    suspend fun getDiaries(start: String, end: String) {
+        kotlin.runCatching {
+            diaryRepository.getDiaries(start, end)
+        }.fold({
+            Log.e("message", it.getOrNull().toString())
+            _responseDiaries.value = it.getOrNull()
+        }, {
+            Log.e("message", it.message.toString())
+        })
     }
 
-    fun getDateDiary(date: String) {
-        viewModelScope.launch {
-            kotlin.runCatching {
-                diaryRepository.getDiaries(start = date, end = date)
-            }.fold({
-                _responseDateDiary.value = it.getOrNull()?.diaries?.values?.firstOrNull()
-            }, {
-                Log.e("message", it.message.toString())
-            })
-        }
+    suspend fun getDateDiary(date: String) {
+        kotlin.runCatching {
+            diaryRepository.getDiaries(start = date, end = date)
+        }.fold({
+            _responseDateDiary.value = it.getOrNull()?.diaries?.values?.firstOrNull()
+        }, {
+            Log.e("message", it.message.toString())
+        })
     }
 
     fun setBadgeInfo(name: String, imageUrl: String, isFirst: Boolean) {
