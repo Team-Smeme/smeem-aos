1c51930cfe10089a5af08764497eeb71989df8ac
watermin <tnals08019@gmail.com>
2023-08-26 10:01:32 +0900

    ui-34 : 온보딩 이용약관 뷰에서 웹뷰로 이동하는 로직 추가

--- app/src/main/java/com/sopt/smeem/presentation/agreement/AgreementViewActivity.kt
+++ app/src/main/java/com/sopt/smeem/presentation/agreement/AgreementViewActivity.kt
@@ -1,34 +1,41 @@
 package com.sopt.smeem.presentation.agreement
 
-import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import android.webkit.WebChromeClient
-import android.webkit.WebResourceRequest
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import com.sopt.smeem.R
 import com.sopt.smeem.databinding.ActivityAgreementViewBinding
 import com.sopt.smeem.presentation.BindingActivity
 
-class AgreementViewActivity : BindingActivity<ActivityAgreementViewBinding>(R.layout.activity_agreement_view) {
+class AgreementViewActivity :
+    BindingActivity<ActivityAgreementViewBinding>(R.layout.activity_agreement_view) {
     override fun constructLayout() {
         initWebView()
         startWebView()
     }
 
     private fun initWebView() {
+        binding.wvAgreement.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(view: WebView, url: String): Boolean {
+                view.loadUrl(url)
+                return true
+            }
+
+            override fun onPageFinished(view: WebView?, url: String?) {
+                super.onPageFinished(view, url)
+            }
+        }
+
         binding.wvAgreement.settings.apply {
             javaScriptEnabled = true
             loadWithOverviewMode = true
             useWideViewPort = true
+            domStorageEnabled = true
         }
     }
 
     private fun startWebView() {
-//        intent.getStringExtra("agreement")?.let {
-//            binding.wvAgreement.loadUrl(it)
-//        }
-        binding.wvAgreement.loadUrl(resources.getString(R.string.personal_info_agreement))
+        intent.getStringExtra("agreement")?.let {
+            binding.wvAgreement.loadUrl(it)
+        }
     }
 }
