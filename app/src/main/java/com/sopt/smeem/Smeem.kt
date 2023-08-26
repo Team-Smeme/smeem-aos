010db9824bde54496d531e79c5b3bbd3481c9f1c
HaeTi <t9h9@naver.com>
2023-08-26 22:44:58 +0900

    Timber 추가

--- app/src/main/java/com/sopt/smeem/Smeem.kt
+++ app/src/main/java/com/sopt/smeem/Smeem.kt
@@ -4,12 +4,14 @@ import android.app.Application
 import com.kakao.sdk.common.KakaoSdk
 import com.sopt.smeem.BuildConfig.KAKAO_API_KEY
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
 class Smeem : Application() {
     override fun onCreate() {
         super.onCreate()
 
+        if (BuildConfig.DEBUG) Timber.plant(Timber.DebugTree())
         KakaoSdk.init(this, KAKAO_API_KEY)
     }
 }
