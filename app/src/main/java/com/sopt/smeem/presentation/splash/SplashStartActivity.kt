24d747159cc9d82df9dda9ad44e2fdb6997bd9b9
watermin <tnals08019@gmail.com>
2023-09-04 14:14:17 +0900

    remote config 파라미터 값과 버전 비교 로직 구현

--- app/src/main/java/com/sopt/smeem/presentation/splash/SplashStartActivity.kt
+++ app/src/main/java/com/sopt/smeem/presentation/splash/SplashStartActivity.kt
@@ -6,9 +6,16 @@ import android.view.WindowManager
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings
+import com.google.firebase.remoteconfig.ktx.remoteConfig
+import com.google.firebase.remoteconfig.ktx.remoteConfigSettings
+import com.sopt.smeem.BuildConfig
 import com.sopt.smeem.R
 import com.sopt.smeem.presentation.home.HomeActivity
 import dagger.hilt.android.AndroidEntryPoint
+import timber.log.Timber
 
 @AndroidEntryPoint
 class SplashStartActivity() : AppCompatActivity() {
@@ -27,6 +34,7 @@ class SplashStartActivity() : AppCompatActivity() {
     }
 
     fun constructLayout() {
+        checkVersion()
         vm.checkAuthed()
     }
 
@@ -37,6 +45,46 @@ class SplashStartActivity() : AppCompatActivity() {
     private fun setStatusBarColor() {
         window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
         window.statusBarColor = getColor(R.color.point)
+    }
+
+    private fun checkVersion() {
+        val configSettings = remoteConfigSettings {
+            minimumFetchIntervalInSeconds = 3600
+        }.toBuilder().build()
+
+        Firebase.remoteConfig.apply {
+            setConfigSettingsAsync(configSettings)
+            setDefaultsAsync(R.xml.remote_config_defaults)
+            fetch().addOnCompleteListener(this@SplashStartActivity) { task ->
+                val installedVersion = BuildConfig.VERSION_NAME.replace(".", "@")
+                val firebaseVersion =
+                    getString("app_version").takeIf { it.isNotEmpty() }?.replace(".", "@")
+                val isNewVersion = when {
+                    firebaseVersion.isNullOrEmpty() -> false
+                    else -> {
+                        val installedVersionParts = installedVersion.split("@").map { it.toInt() }
+                        val firebaseVersionParts = firebaseVersion.split("@").map { it.toInt() }
+                        installedVersionParts.zip(firebaseVersionParts)
+                            .all { (installed, firebase) ->
+                                installed >= firebase
+                            }
+                    }
+                }
+
+                if (task.isSuccessful) {
+                    activate()
+                    if (isNewVersion) {
+                        Timber.tag("smeem is updated to a new version")
+                            .d("%s | %s", installedVersion, firebaseVersion)
+                    } else {
+                        Timber.tag("smeem needs to be updated!")
+                            .d("%s | %s", installedVersion, firebaseVersion)
+                    }
+                } else {
+                    Timber.e("remote config failed")
+                }
+            }
+        }
     }
 
     private fun observeAuthed() {
