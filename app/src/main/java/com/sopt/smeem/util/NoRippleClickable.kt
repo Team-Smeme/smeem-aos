319f52073620c3bd1bb1ca3704c4ee4117e132e3
HaeTi <t9h9@naver.com>
2024-02-21 17:13:34 +0900

    noRippleClickable 확장함수 생성

---
+++ app/src/main/java/com/sopt/smeem/util/NoRippleClickable.kt
@@ -0,0 +1,19 @@
+package com.sopt.smeem.util
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
+
+inline fun Modifier.noRippleClickable(
+    modifier: Modifier = Modifier,
+    crossinline onClick: () -> Unit,
+): Modifier = composed {
+    clickable(
+        indication = null,
+        interactionSource = remember { MutableInteractionSource() },
+    ) {
+        onClick()
+    }
+}
