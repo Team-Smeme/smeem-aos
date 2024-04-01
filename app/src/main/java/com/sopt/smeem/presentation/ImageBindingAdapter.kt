528e5760a13204a37c411baaa6c742433c50f3de
HaeTi <t9h9@naver.com>
2024-04-01 21:07:36 +0900

    placeholder 및 preloading

--- app/src/main/java/com/sopt/smeem/presentation/ImageBindingAdapter.kt
+++ app/src/main/java/com/sopt/smeem/presentation/ImageBindingAdapter.kt
@@ -2,7 +2,10 @@ package com.sopt.smeem.presentation
 
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
+import coil.imageLoader
 import coil.load
+import coil.request.ImageRequest
+import com.sopt.smeem.R
 
 class ImageBindingAdapter {
 
@@ -10,7 +13,22 @@ class ImageBindingAdapter {
         @JvmStatic
         @BindingAdapter(value = ["imgUrl"])
         fun load(imageView: ImageView, imageUrl: String?) {
-            imageView.load(imageUrl)
+            imageView.load(imageUrl) {
+                crossfade(true)
+                placeholder(R.drawable.img_badge_placeholder)
+            }
+        }
+
+        @JvmStatic
+        @BindingAdapter("imageUrlPreload")
+        fun ImageView.preloadImage(url: String?) {
+            url?.let {
+                val request = ImageRequest.Builder(context)
+                    .data(it)
+                    .build()
+
+                context.imageLoader.enqueue(request)
+            }
         }
 
         @JvmStatic
