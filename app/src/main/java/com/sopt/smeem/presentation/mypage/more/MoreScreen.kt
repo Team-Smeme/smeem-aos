7e401a6d50614f44a6415fa93d038d43ea817365
HaeTi <t9h9@naver.com>
2024-04-16 02:21:19 +0900

    더보기 화면 추가

---
+++ app/src/main/java/com/sopt/smeem/presentation/mypage/more/MoreScreen.kt
@@ -0,0 +1,25 @@
+package com.sopt.smeem.presentation.mypage.more
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+
+@Composable
+fun MoreScreen(
+    navController: NavController,
+    modifier: Modifier
+) {
+    Column(
+        modifier = modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(text = "더보기 화면이다")
+
+    }
+}
--- app/src/main/java/com/sopt/smeem/presentation/mypage/navigation/MyPageScreen.kt
+++ app/src/main/java/com/sopt/smeem/presentation/mypage/navigation/MyPageScreen.kt
@@ -1,9 +1,11 @@
 package com.sopt.smeem.presentation.mypage.navigation
 
+import com.sopt.smeem.presentation.mypage.MORE
 import com.sopt.smeem.presentation.mypage.MY_SUMMARY
 import com.sopt.smeem.presentation.mypage.SETTING
 
 sealed class MyPageScreen(val route: String) {
     data object MySummary : MyPageScreen(MY_SUMMARY)
     data object Setting : MyPageScreen(SETTING)
+    data object More : MyPageScreen(MORE)
 }
