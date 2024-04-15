b79fcf3d323ae901d085191d87bfe1ab162373f9
HaeTi <t9h9@naver.com>
2024-04-16 00:22:31 +0900

    성과요약 TopAppBar 구현

---
+++ app/src/main/java/com/sopt/smeem/presentation/mypage/components/MyPageTopAppBars.kt
@@ -0,0 +1,73 @@
+package com.sopt.smeem.presentation.mypage.components
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.sopt.smeem.R
+import com.sopt.smeem.presentation.home.calendar.ui.theme.Typography
+import com.sopt.smeem.presentation.home.calendar.ui.theme.gray500
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MySummaryTopAppBar(
+    onNavigationIconClick: () -> Unit = {},
+    onSettingClick: () -> Unit = {}
+) {
+    CenterAlignedTopAppBar(
+        modifier = Modifier
+            .fillMaxWidth(),
+        colors = TopAppBarDefaults.centerAlignedTopAppBarColors().copy(
+            containerColor = Color.White
+        ),
+        title = {
+            Text(
+                text = "성과 요약",
+                style = Typography.titleMedium
+            )
+        },
+        navigationIcon = {
+            IconButton(
+                modifier = Modifier.padding(start = 6.dp),
+                onClick = onNavigationIconClick
+            ) {
+                Icon(
+                    imageVector = ImageVector.vectorResource(id = R.drawable.ic_arrow_back),
+                    contentDescription = "Back"
+                )
+            }
+        },
+        actions = {
+            IconButton(
+                modifier = Modifier.padding(end = 16.dp),
+                onClick = onSettingClick
+            ) {
+                Text(
+                    text = "설정",
+                    style = Typography.bodyMedium.copy(
+                        color = gray500,
+                        fontWeight = FontWeight.SemiBold
+                    ),
+                )
+            }
+        }
+    )
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun MySummaryTopAppBarPreview() {
+    MySummaryTopAppBar()
+}
