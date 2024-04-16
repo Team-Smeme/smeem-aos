657a00553784bca646802bc2cdfeb40dfc992c19
HaeTi <t9h9@naver.com>
2024-04-17 01:23:35 +0900

    SmeemContents 구현 (Title + content)

---
+++ app/src/main/java/com/sopt/smeem/presentation/mypage/components/SmeemContents.kt
@@ -0,0 +1,30 @@
+package com.sopt.smeem.presentation.mypage.components
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.sopt.smeem.presentation.home.calendar.ui.theme.Typography
+import com.sopt.smeem.util.VerticalSpacer
+
+@Composable
+fun SmeemContents(
+    modifier: Modifier = Modifier,
+    title: String,
+    content: @Composable () -> Unit
+) {
+    Column(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(horizontal = 18.dp)
+    ) {
+        Text(text = title, style = Typography.titleLarge)
+
+        VerticalSpacer(height = 12.dp)
+
+        content()
+    }
+}
