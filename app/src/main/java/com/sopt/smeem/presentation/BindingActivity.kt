15c2afd019632f20d7e144bda1e1be9fc49e5868
HaeTi <t9h9@naver.com>
2024-04-19 02:32:28 +0900

    base animation 적용

--- app/src/main/java/com/sopt/smeem/presentation/BindingActivity.kt
+++ app/src/main/java/com/sopt/smeem/presentation/BindingActivity.kt
@@ -1,13 +1,12 @@
 package com.sopt.smeem.presentation
 
+import android.os.Build
 import android.os.Bundle
-import android.view.MotionEvent
-import android.view.View
 import androidx.annotation.LayoutRes
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
-import com.sopt.smeem.util.hideKeyboard
+import com.sopt.smeem.R
 
 abstract class BindingActivity<Binding : ViewDataBinding>(
     @LayoutRes private val layoutRes: Int,
@@ -16,12 +15,38 @@ abstract class BindingActivity<Binding : ViewDataBinding>(
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        createOpenAnimation()
         binding = DataBindingUtil.setContentView(this, layoutRes)
         binding.lifecycleOwner = this
 
         constructLayout()
         addListeners()
         addObservers()
+    }
+
+    override fun finish() {
+        super.finish()
+        createCloseAnimation()
+    }
+
+    private fun createOpenAnimation() {
+        if (Build.VERSION.SDK_INT >= 34) {
+            overrideActivityTransition(
+                OVERRIDE_TRANSITION_OPEN, R.anim.fade_in, R.anim.fade_out
+            )
+        } else {
+            overridePendingTransition(R.anim.fade_in, R.anim.fade_out)
+        }
+    }
+
+    private fun createCloseAnimation() {
+        if (Build.VERSION.SDK_INT >= 34) {
+            overrideActivityTransition(
+                OVERRIDE_TRANSITION_CLOSE, R.anim.fade_in, R.anim.fade_out
+            )
+        } else {
+            overridePendingTransition(R.anim.fade_in, R.anim.fade_out)
+        }
     }
 
     open fun constructLayout() {}
