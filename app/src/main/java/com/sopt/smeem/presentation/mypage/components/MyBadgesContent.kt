73a00eea0f87114440d65b482676790b274428f1
wateralsie <tnals08019@gmail.com>
2024-04-19 22:59:37 +0900

    배지 미획득 카드 제작

--- app/src/main/java/com/sopt/smeem/presentation/mypage/components/MyBadgesContent.kt
+++ app/src/main/java/com/sopt/smeem/presentation/mypage/components/MyBadgesContent.kt
@@ -1,12 +1,21 @@
 package com.sopt.smeem.presentation.mypage.components
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -54,7 +63,24 @@ fun MyBadgesNotObtainedCard(
     info: MyBadges,
     modifier: Modifier = Modifier
 ) {
-
+    Card(
+        modifier = modifier.aspectRatio(1f),
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.surface,
+        ),
+        shape = RoundedCornerShape(10.dp)
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+            modifier = Modifier.fillMaxSize(),
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.ic_badge_lock),
+                contentDescription = null
+            )
+        }
+    }
 }
 
 @Preview
@@ -62,5 +88,13 @@ fun MyBadgesNotObtainedCard(
 fun MyBadgesPreview() {
     SmeemTheme {
         MyBadgesContent(badges = BadgeList.sprint2)
+    }
+}
+
+@Preview(widthDp = 200)
+@Composable
+fun MyBadgesNotObtainedCardPreview() {
+    SmeemTheme {
+        MyBadgesNotObtainedCard(info = BadgeList.sprint2[2])
     }
 }
