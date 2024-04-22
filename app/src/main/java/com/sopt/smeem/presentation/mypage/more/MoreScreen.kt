b00357e10c315c6e011f9321beec0244af9228dc
HaeTi <t9h9@naver.com>
2024-04-23 01:19:25 +0900

    더보기 화면 구현

--- app/src/main/java/com/sopt/smeem/presentation/mypage/more/MoreScreen.kt
+++ app/src/main/java/com/sopt/smeem/presentation/mypage/more/MoreScreen.kt
@@ -1,13 +1,23 @@
 package com.sopt.smeem.presentation.mypage.more
 
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import com.sopt.smeem.R
+import com.sopt.smeem.presentation.theme.Typography
+import com.sopt.smeem.presentation.theme.black
+import com.sopt.smeem.presentation.theme.gray600
+import com.sopt.smeem.util.VerticalSpacer
 
 @Composable
 fun MoreScreen(
@@ -15,11 +25,74 @@ fun MoreScreen(
     modifier: Modifier
 ) {
     Column(
-        modifier = modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
+        modifier = modifier
+            .fillMaxSize()
+            .padding(horizontal = 24.dp),
     ) {
-        Text(text = "더보기 화면이다")
+
+        VerticalSpacer(height = 30.dp)
+
+        Text(
+            text = stringResource(R.string.my_page_more_information),
+            style = Typography.titleMedium,
+            color = black
+        )
+
+        VerticalSpacer(height = 9.dp)
+
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 12.dp, horizontal = 8.dp)
+        ) {
+            Text(
+                text = stringResource(R.string.my_page_more_instructions),
+                style = Typography.bodyMedium,
+                color = gray600
+            )
+        }
+
+        VerticalSpacer(height = 60.dp)
+
+        Text(
+            text = stringResource(R.string.my_page_more_account_management),
+            style = Typography.titleMedium,
+            color = black
+        )
+
+        VerticalSpacer(height = 10.dp)
+
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 12.dp, horizontal = 8.dp)
+        ) {
+            Text(
+                text = stringResource(R.string.my_page_more_logout),
+                style = Typography.bodyMedium,
+                color = gray600
+            )
+        }
+
+        VerticalSpacer(height = 6.dp)
+
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 12.dp, horizontal = 8.dp)
+        ) {
+            Text(
+                text = stringResource(R.string.my_page_more_delete_account),
+                style = Typography.bodyMedium,
+                color = gray600
+            )
+        }
 
     }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun MoreScreenPreview() {
+    MoreScreen(navController = rememberNavController(), modifier = Modifier)
 }
