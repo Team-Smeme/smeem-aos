9891aeb5e83f801e4a3e81c1af83aa009386b9a5
HaeTi <t9h9@naver.com>
2024-04-22 23:10:21 +0900

    SmeemTextField 구현

---
+++ app/src/main/java/com/sopt/smeem/presentation/compose/components/SmeemTextField.kt
@@ -0,0 +1,52 @@
+package com.sopt.smeem.presentation.compose.components
+
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.sopt.smeem.presentation.compose.theme.Typography
+import com.sopt.smeem.presentation.compose.theme.gray100
+import com.sopt.smeem.presentation.compose.theme.point
+
+// 공백이나 11자 초과시 버튼 비활성화
+
+@Composable
+fun SmeemTextField(
+    modifier: Modifier = Modifier,
+    text: String,
+    keyboardOptions: KeyboardOptions = KeyboardOptions.Default,
+    onValueChange: (String) -> Unit
+) {
+    BasicTextField(
+        value = text,
+        onValueChange = onValueChange,
+        singleLine = true,
+        keyboardOptions = KeyboardOptions(
+            keyboardType = keyboardOptions.keyboardType,
+            imeAction = ImeAction.Done
+        ),
+        modifier = modifier
+            .fillMaxWidth()
+            .border(width = 1.dp, color = gray100)
+            .padding(vertical = 19.dp, horizontal = 20.dp),
+        textStyle = Typography.headlineSmall.copy(
+            color = point
+        )
+
+    )
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun SmeemTextFieldPreview() {
+    SmeemTextField(
+        text = "텍스트",
+        onValueChange = {}
+    )
+}
--- app/src/main/java/com/sopt/smeem/presentation/mypage/setting/ChangeNicknameScreen.kt
+++ app/src/main/java/com/sopt/smeem/presentation/mypage/setting/ChangeNicknameScreen.kt
@@ -1,14 +1,45 @@
 package com.sopt.smeem.presentation.mypage.setting
 
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.sopt.smeem.presentation.compose.components.SmeemTextField
+import com.sopt.smeem.util.VerticalSpacer
 
 @Composable
 fun ChangeNicknameScreen(
     modifier: Modifier = Modifier,
     nickname: String
 ) {
-    Text(text = "ChangeNicknameScreen: $nickname", modifier = modifier)
+    var newNickName by rememberSaveable { mutableStateOf(nickname) }
 
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        VerticalSpacer(height = 14.dp)
+
+        SmeemTextField(
+            text = newNickName,
+            onValueChange = { newNickName = it },
+            modifier = Modifier.padding(horizontal = 18.dp)
+        )
+
+    }
+
+}
+
+@Composable
+@Preview(showBackground = true, showSystemUi = true)
+fun ChangeNicknameScreenPreview() {
+    ChangeNicknameScreen(nickname = "이태하이")
 }
