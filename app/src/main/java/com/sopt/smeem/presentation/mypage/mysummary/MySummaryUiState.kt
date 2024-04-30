b3146d35322f834741af9fd1fd05e458bc367de2
HaeTi <t9h9@naver.com>
2024-05-01 00:44:03 +0900

    MySummaryUiState 생성

---
+++ app/src/main/java/com/sopt/smeem/presentation/mypage/mysummary/MySummaryUiState.kt
@@ -0,0 +1,15 @@
+package com.sopt.smeem.presentation.mypage.mysummary
+
+import com.sopt.smeem.domain.dto.MyPlanDto
+import com.sopt.smeem.domain.dto.MySmeemDataDto
+
+sealed class MySummaryUiState {
+    data object Loading : MySummaryUiState()
+    data class Success(
+        val smeemData: MySmeemDataDto,
+        val planData: MyPlanDto?,
+//        val badgesData: List<GetBadgeListDto>
+    ) : MySummaryUiState()
+
+    data class Error(val message: String) : MySummaryUiState()
+}
