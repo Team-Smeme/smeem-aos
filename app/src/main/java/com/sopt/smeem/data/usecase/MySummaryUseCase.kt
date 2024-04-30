c6d516109b4179805ee4822b0a391ed896e3e81e
HaeTi <t9h9@naver.com>
2024-05-01 00:44:41 +0900

    MySummaryUseCase 생성

---
+++ app/src/main/java/com/sopt/smeem/data/usecase/MySummaryUseCase.kt
@@ -0,0 +1,27 @@
+package com.sopt.smeem.data.usecase
+
+import com.sopt.smeem.domain.dto.MyPlanDto
+import com.sopt.smeem.domain.dto.MySmeemDataDto
+import com.sopt.smeem.domain.repository.UserRepository
+import kotlinx.coroutines.coroutineScope
+import javax.inject.Inject
+
+class MySummaryUseCase @Inject constructor(
+    private val userRepository: UserRepository
+) {
+
+    // TODO : 배지까지 반영 -> 주석 해제
+//    suspend operator fun invoke(): Triple<MySmeemDataDto, MyPlanDto?, List<GetBadgeListDto>> =
+//        coroutineScope {
+//            val smeemData = userRepository.getMySmeemData().data()
+//            val myPlan = userRepository.getMyPlanData().data()
+//            val badgeList = userRepository.getMyBadges().data()
+//            Triple(smeemData, myPlan, badgeList)
+//        }
+
+    suspend operator fun invoke(): Pair<MySmeemDataDto, MyPlanDto?> = coroutineScope {
+        val smeemData = userRepository.getMySmeemData().data()
+        val myPlan = userRepository.getMyPlanData().data()
+        Pair(smeemData, myPlan)
+    }
+}
