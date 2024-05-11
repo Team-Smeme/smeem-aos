99207a115844ee70a90e10ca0dc15211216d77d8
HaeTi <t9h9@naver.com>
2024-05-11 13:20:34 +0900

    TempMyPageActivity에도 전환 애니메이션 반영

--- app/src/main/java/com/sopt/smeem/presentation/mypage/TempMyPageActivity.kt
+++ app/src/main/java/com/sopt/smeem/presentation/mypage/TempMyPageActivity.kt
@@ -1,9 +1,11 @@
 package com.sopt.smeem.presentation.mypage
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.navigation.compose.rememberNavController
+import com.sopt.smeem.R
 import com.sopt.smeem.presentation.compose.theme.SmeemTheme
 import com.sopt.smeem.presentation.mypage.navigation.MyPageNavHost
 import dagger.hilt.android.AndroidEntryPoint
@@ -12,6 +14,7 @@ import dagger.hilt.android.AndroidEntryPoint
 class TempMyPageActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        createOpenAnimation()
         setContent {
             SmeemTheme {
                 val navController = rememberNavController()
@@ -19,4 +22,31 @@ class TempMyPageActivity : ComponentActivity() {
             }
         }
     }
+
+    override fun onDestroy() {
+        createCloseAnimation()
+        super.onDestroy()
+    }
+
+    private fun createOpenAnimation() {
+        if (Build.VERSION.SDK_INT >= 34) {
+            overrideActivityTransition(
+                OVERRIDE_TRANSITION_OPEN, R.anim.fade_in, R.anim.fade_out
+            )
+        } else {
+            overridePendingTransition(R.anim.fade_in, R.anim.fade_out)
+        }
+    }
+
+    private fun createCloseAnimation() {
+        if (Build.VERSION.SDK_INT >= 34) {
+            overrideActivityTransition(
+                OVERRIDE_TRANSITION_CLOSE, R.anim.fade_in, R.anim.fade_out
+            )
+        } else {
+            overridePendingTransition(R.anim.fade_in, R.anim.fade_out)
+        }
+    }
+
 }
+
