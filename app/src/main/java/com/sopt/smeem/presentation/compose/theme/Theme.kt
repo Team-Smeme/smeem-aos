f8cc9488af58619d1cb4b678546f4880a8633b85
HaeTi <t9h9@naver.com>
2024-05-11 13:52:54 +0900

    Compose theme에서 status bar 색 변경 제거 및 아이콘 색상 변경

--- app/src/main/java/com/sopt/smeem/presentation/compose/theme/Theme.kt
+++ app/src/main/java/com/sopt/smeem/presentation/compose/theme/Theme.kt
@@ -7,7 +7,6 @@ import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
-import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
@@ -46,8 +45,7 @@ fun SmeemTheme(
     if (!view.isInEditMode) {
         SideEffect {
             val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.primary.toArgb()
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !darkTheme
         }
     }
 
