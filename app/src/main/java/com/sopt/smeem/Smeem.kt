7af81ca16e0756e62d720c7f68a683f2360b1b62
HaeTi <t9h9@naver.com>
2024-05-21 23:15:48 +0900

    appsflyer 적용

--- app/src/main/java/com/sopt/smeem/Smeem.kt
+++ app/src/main/java/com/sopt/smeem/Smeem.kt
@@ -4,7 +4,10 @@ import android.app.Application
 import androidx.appcompat.app.AppCompatDelegate
 import com.amplitude.android.Amplitude
 import com.amplitude.android.Configuration
+import com.appsflyer.AppsFlyerLib
+import com.appsflyer.deeplink.DeepLinkResult
 import com.kakao.sdk.common.KakaoSdk
+import com.sopt.smeem.BuildConfig.APPFLYER_DEV_KEY
 import com.sopt.smeem.BuildConfig.DEV_AMPLITUDE_API_KEY
 import com.sopt.smeem.BuildConfig.KAKAO_API_KEY
 import com.sopt.smeem.BuildConfig.PROD_AMPLITUDE_API_KEY
@@ -25,6 +28,35 @@ class Smeem : Application() {
         if (BuildConfig.DEBUG) Timber.plant(Timber.DebugTree())
         KakaoSdk.init(this, KAKAO_API_KEY)
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        setOnelink()
+    }
+
+    private fun setOnelink() {
+        val appsFlyer = AppsFlyerLib.getInstance()
+        appsFlyer.init(APPFLYER_DEV_KEY, null, this)
+        appsFlyer.subscribeForDeepLink { deepLinkResult ->
+            when (deepLinkResult.status) {
+                DeepLinkResult.Status.FOUND -> {
+                    Timber.e("DeepLinking: ${deepLinkResult.deepLink}")
+                }
+
+                DeepLinkResult.Status.NOT_FOUND -> {
+                    Timber.e("DeepLinking: NOT_FOUND")
+                }
+
+                DeepLinkResult.Status.ERROR -> {
+                    val error = deepLinkResult.error
+                    Timber.e("DeepLinking: ERROR $error")
+                }
+            }
+            val deepLink = deepLinkResult.deepLink
+            try {
+                Timber.e("DeepLinking: $deepLink")
+            } catch (e: Exception) {
+                Timber.e("DeepLinking: $e")
+
+            }
+        }
     }
 
     companion object {
