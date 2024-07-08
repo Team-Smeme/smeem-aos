0045060aa54cde0d65aa2f3f5b21982d5e30a00b
HaeTi <t9h9@naver.com>
2024-07-09 01:06:52 +0900

    DataStore에 Banner version, Banner Closed 변수 추가

--- app/src/main/java/com/sopt/smeem/data/SmeemDataStore.kt
+++ app/src/main/java/com/sopt/smeem/data/SmeemDataStore.kt
@@ -3,6 +3,8 @@ package com.sopt.smeem.data
 import android.content.Context
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.booleanPreferencesKey
+import androidx.datastore.preferences.core.intPreferencesKey
 import androidx.datastore.preferences.core.stringPreferencesKey
 import androidx.datastore.preferences.preferencesDataStore
 
@@ -10,4 +12,6 @@ object SmeemDataStore {
     val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "storage")
 
     val RECENT_DIARY_DATE = stringPreferencesKey("recent_diary_date")
+    val BANNER_CLOSED = booleanPreferencesKey("banner_closed")
+    val BANNER_VERSION = intPreferencesKey("banner_version")
 }
