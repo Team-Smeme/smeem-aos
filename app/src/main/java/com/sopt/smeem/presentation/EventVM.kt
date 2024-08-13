149407960d32332a8bbd3d25e54eaccb8a1288e0
HaeTi <t9h9@naver.com>
2024-08-14 02:07:44 +0900

    엠플리튜드에 property도 보낼 수 있게 확장

--- app/src/main/java/com/sopt/smeem/presentation/EventVM.kt
+++ app/src/main/java/com/sopt/smeem/presentation/EventVM.kt
@@ -1,6 +1,5 @@
 package com.sopt.smeem.presentation
 
-import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.sopt.smeem.Smeem.Companion.AMPLITUDE
@@ -11,11 +10,13 @@ import timber.log.Timber
 import javax.inject.Inject
 
 @HiltViewModel
-class EventVM @Inject constructor(): ViewModel() {
-    fun sendEvent(event: AmplitudeEventType) {
+class EventVM @Inject constructor() : ViewModel() {
+    fun sendEvent(event: AmplitudeEventType, properties: Map<String, Any>? = null) {
         try {
             viewModelScope.launch {
-                AMPLITUDE.track(event.eventName)
+                if (properties != null) {
+                    AMPLITUDE.track(event.eventName, properties)
+                } else AMPLITUDE.track(event.eventName)
             }
         } catch (t: Throwable) {
             // 이벤트 발송이 기존 로직에 영향은 없도록
