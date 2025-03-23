42cb3a72f6197441cbb1f01ab61fc28a8cd1cc08
haeti-dev <t9h9@soongsil.ac.kr>
2025-03-23 23:47:26 +0900

    feat : 코칭 응답에 username, totalCount 추가

--- app/src/main/java/com/sopt/smeem/domain/dto/CorrectionDto.kt
+++ app/src/main/java/com/sopt/smeem/domain/dto/CorrectionDto.kt
@@ -1,5 +1,11 @@
 package com.sopt.smeem.domain.dto
 
+data class CorrectionsResponseDto(
+    val corrections: List<CorrectionDto>,
+    val username: String,
+    val totalCount: Int,
+)
+
 data class CorrectionDto(
     val originalSentence: String,
     val correctedSentence: String,
