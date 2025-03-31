c1159b4c53136a0063ea8f789c15781f70076e6f
haeti-dev <t9h9@soongsil.ac.kr>
2025-03-31 23:56:41 +0900

    feat : SmeemButton에 throttle 속성 부여

--- app/src/main/java/com/sopt/smeem/presentation/compose/components/SmeemButton.kt
+++ app/src/main/java/com/sopt/smeem/presentation/compose/components/SmeemButton.kt
@@ -7,6 +7,11 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -14,16 +19,32 @@ import com.sopt.smeem.presentation.compose.theme.Typography
 import com.sopt.smeem.presentation.compose.theme.point
 import com.sopt.smeem.presentation.compose.theme.pointInactive
 import com.sopt.smeem.presentation.compose.theme.white
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 @Composable
 fun SmeemButton(
     modifier: Modifier = Modifier,
     text: String,
     onClick: () -> Unit,
-    isButtonEnabled: Boolean = true
+    isButtonEnabled: Boolean = true,
+    throttleTime: Long = 300L
 ) {
+    var isThrottling by rememberSaveable { mutableStateOf(false) }
+    val coroutineScope = rememberCoroutineScope()
+
     Button(
-        onClick = onClick,
+        onClick = {
+            if (!isThrottling) {
+                isThrottling = true
+                onClick()
+
+                coroutineScope.launch {
+                    delay(throttleTime)
+                    isThrottling = false
+                }
+            }
+        },
         modifier = modifier.fillMaxWidth(),
         enabled = isButtonEnabled,
         shape = RoundedCornerShape(6.dp),
