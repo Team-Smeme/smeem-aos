1e41d2a059a79219802252f153502c01678ee0ca
haeti-dev <t9h9@soongsil.ac.kr>
2025-04-08 22:15:37 +0900

    fix : 더보기 간격 수정

--- app/src/main/java/com/sopt/smeem/presentation/mypage/more/MoreScreen.kt
+++ app/src/main/java/com/sopt/smeem/presentation/mypage/more/MoreScreen.kt
@@ -2,14 +2,12 @@ package com.sopt.smeem.presentation.mypage.more
 
 import android.app.Activity
 import android.content.Intent
-import android.net.Uri
 import androidx.browser.customtabs.CustomTabsIntent
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
@@ -19,14 +17,13 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.core.net.toUri
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.sopt.smeem.R
 import com.sopt.smeem.presentation.compose.components.SmeemDialog
 import com.sopt.smeem.presentation.compose.theme.Typography
 import com.sopt.smeem.presentation.compose.theme.black
-import com.sopt.smeem.presentation.compose.theme.gray100
 import com.sopt.smeem.presentation.compose.theme.gray600
 import com.sopt.smeem.presentation.mypage.navigation.MoreNavGraph
 import com.sopt.smeem.presentation.splash.SplashLoginActivity
@@ -37,7 +34,7 @@ fun MoreScreen(
     navController: NavController,
     modifier: Modifier = Modifier
 ) {
-    val viewModel: MoreViewModel = hiltViewModel()
+//    val viewModel: MoreViewModel = hiltViewModel()
     val context = LocalContext.current
 
     val (showLogoutDialog, setShowLogoutDialog) = rememberSaveable { mutableStateOf(false) }
@@ -48,7 +45,7 @@ fun MoreScreen(
             title = stringResource(R.string.smeem_dialog_logout_title),
             content = stringResource(R.string.smeem_dialog_logout_content),
             onConfirmButtonClick = {
-                viewModel.clearLocal()
+//                viewModel.clearLocal()
                 context.startActivity(Intent(context, SplashLoginActivity::class.java))
                 (context as? Activity)?.finishAffinity()
             })
@@ -72,20 +69,19 @@ fun MoreScreen(
         VerticalSpacer(height = 9.dp)
 
 
-        Box(modifier = Modifier
-            .clickable {
-                CustomTabsIntent
-                    .Builder()
-                    .build()
-                    .run {
-                        launchUrl(
-                            context, Uri.parse(
-                                context.getString(R.string.manual_url)
+        Box(
+            modifier = Modifier
+                .clickable {
+                    CustomTabsIntent
+                        .Builder()
+                        .build()
+                        .run {
+                            launchUrl(
+                                context, context.getString(R.string.manual_url).toUri()
                             )
-                        )
-                    }
-            }
-            .padding(vertical = 12.dp, horizontal = 8.dp)
+                        }
+                }
+                .padding(vertical = 12.dp)
         ) {
             Text(
                 text = stringResource(R.string.my_page_more_manual),
@@ -94,14 +90,7 @@ fun MoreScreen(
             )
         }
 
-        VerticalSpacer(height = 30.dp)
-
-        HorizontalDivider(
-            color = gray100,
-            thickness = 1.dp,
-        )
-
-        VerticalSpacer(height = 30.dp)
+        VerticalSpacer(height = 20.dp)
 
         Text(
             text = stringResource(R.string.my_page_more_account_management),
@@ -114,7 +103,7 @@ fun MoreScreen(
         Box(
             modifier = Modifier
                 .clickable { setShowLogoutDialog(true) }
-                .padding(vertical = 12.dp, horizontal = 8.dp)
+                .padding(vertical = 12.dp)
         ) {
             Text(
                 text = stringResource(R.string.my_page_more_logout),
@@ -130,7 +119,7 @@ fun MoreScreen(
                 .clickable {
                     navController.navigate(MoreNavGraph.DeleteAccount.route)
                 }
-                .padding(vertical = 12.dp, horizontal = 8.dp)
+                .padding(vertical = 12.dp)
         ) {
             Text(
                 text = stringResource(R.string.my_page_more_delete_account),
